d22909b6d533242512191ee271e5242a018177cb
Gary J <github@accu.ca>
2022-05-16 21:19:19 -0700

    Update

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const App = () => {
   const list = friends.map(friend => {
     return (
       <View key={friend.id} style={styles.listItem}>
-        <Text style={styles.text}>{friend.name}</Text>
+        <Text style={styles.itemText}>{friend.name}</Text>
         <View style={styles.separator}></View>
       </View>
     );
@@ -21,6 +21,7 @@ const App = () => {
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar />
+      <Text style={styles.text}>Hello React</Text>
       <ScrollView>
         {list}
       </ScrollView>
@@ -41,7 +42,8 @@ const styles = StyleSheet.create({
     height: 2,
     backgroundColor: '#EEE',
   },
-  text: {fontSize: 24, fontWeight: '500', color: '#EEE'},
+  text: {fontSize: 24, fontWeight: '600', color: '#555'},
+  itemText: {fontSize: 24, fontWeight: '500', color: '#EEE'},
 });
 
 export default App;
